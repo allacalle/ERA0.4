6ef461e9d872c1a29f190836826956d3f51a8d7a
Alfonso <allacalle@gmail.com>
2016-06-09 12:10:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alfonso/era04/AyudaFormula.java
@@ -0,0 +1,13 @@
+package com.example.alfonso.era04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AyudaFormula extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda_formula);
+    }
+}
---
+++ app/src/main/java/com/example/alfonso/era04/MensajePreEncuesta4.java
@@ -0,0 +1,13 @@
+package com.example.alfonso.era04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MensajePreEncuesta4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensaje_pre_encuesta4);
+    }
+}
